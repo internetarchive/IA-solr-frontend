be3f1e3bcec576d979471ca40b54835856d326f8
Edward Betts <edwardbetts@gmail.com>
2012-01-14 07:13:01 +0000

    reorder facets
    also refactor search.py

--- search.py
+++ search.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request
 from urllib import urlopen, quote_plus
-import json, locale, sys, re
+import json, locale, sys, re, time
 
 from socket import socket, AF_INET, SOCK_DGRAM, SOL_UDP, SO_BROADCAST, timeout
-import re
 
 app = Flask(__name__)
 
@@ -44,6 +43,7 @@ def quote(s):
 solr_select_url = 'http://localhost:8983/solr/select?wt=json' + \
     '&json.nl=arrarr' + \
     '&defType=edismax' + \
+    '&qf=text' + \
     '&fl=identifier,creator,title,date,subject,collection,scanner,mediatype,description,noindex,score' + \
     '&rows=' + str(results_per_page) + \
     '&facet=true&facet.limit=30&facet.mincount=2' + \
@@ -177,59 +177,55 @@ re_date_range = re.compile('^(\d+)-(\d+)$')
 @app.route("/")
 def do_search():
     q = request.args.get('q')
-    results = None
-    pages = None
     pages_in_set = 10
-    half = None
-    first_page_in_set = None
-    last_page_in_set = None
+    if not q:
+        return render_template('search.html')
     facet_args = [(f, request.args[f]) for f in facet_fields if f in request.args]
-    facet_args_dict = dict(facet_args)
-    page = 1
-    if q:
-        quote_q = quote(q)
-        page = int(request.args.get('page', 1))
-        start = results_per_page * (page-1)
-        fq = ''.join('&fq=' + quote('{!tag=%s}%s:"%s"' % (f, f, request.args[f])) for f in facet_fields if f in request.args)
-        date_range = request.args.get('date_range')
-        date_facet = request.args.get('date_facet')
-        if date_range:
-            m = re_date_range.match(date_range)
-            if m:
-                start_year, end_year = m.groups()
-                fq += '&fq=' + quote('date:[%s-01-01T00:00:00Z TO %s-01-01T00:00:00Z]' % (start_year, end_year))
-        elif date_facet:
-            fq += '&fq=' + quote('date:[%s-01-01T00:00:00Z TO %s-01-01T00:00:00Z+%dYEAR]' % (date_facet, date_facet, year_gap))
+    quote_q = quote(q)
+    page = int(request.args.get('page', 1))
+    start = results_per_page * (page-1)
+    fq = ''.join('&fq=' + quote('{!tag=%s}{!term f=%s}%s' % (f, f, request.args[f])) for f in facet_fields if f in request.args)
+    date_range = request.args.get('date_range')
+    date_facet = request.args.get('date_facet')
+    if date_range:
+        m = re_date_range.match(date_range)
+        if m:
+            start_year, end_year = m.groups()
+            fq += '&fq=' + quote('date:[%s-01-01T00:00:00Z TO %s-01-01T00:00:00Z]' % (start_year, end_year))
+    elif date_facet:
+        fq += '&fq=' + quote('date:[%s-01-01T00:00:00Z TO %s-01-01T00:00:00Z+%dYEAR]' % (date_facet, date_facet, year_gap))
 
-        url = solr_select_url + '&q=' + quote(q) + '&start=%d' % start + fq + ''.join('&facet.field='+('{!ex=' + f + '}' if f in facet_args_dict else '') + f for f in facet_fields)
-        f = urlopen(url)
-        reply = f.read()
-        try:
-            results = json.loads(reply)
-        except:
-            print reply
-            raise
+    url = solr_select_url + '&q=' + quote(q) + '&start=%d' % start + fq + ''.join('&facet.field='+('{!ex=' + f + '}' if f in facet_args_dict else '') + f for f in facet_fields)
+    t0_solr = time()
+    f = urlopen(url)
+    reply = f.read()
+    t_solr = time() - t_solr
+    try:
+        results = json.loads(reply)
+    except ValueError:
+        return reply
 
-        num_found = results['response']['numFound']
-        pages = (num_found / results_per_page) + 1
-        half = pages_in_set/2
-        if pages < pages_in_set:
-            first_page_in_set = 1
-            last_page_in_set = pages
-        if page < half:
-            first_page_in_set = 1
-            last_page_in_set = min((pages_in_set, pages))
-        elif page > (pages-half):
-            first_page_in_set = max((pages-pages_in_set, 1))
-            last_page_in_set = pages
-        else:
-            first_page_in_set = max((page-half, 1))
-            last_page_in_set = page+half
+    num_found = results['response']['numFound']
+    pages = (num_found / results_per_page) + 1
+    half = pages_in_set/2
+    if pages < pages_in_set:
+        first_page_in_set = 1
+        last_page_in_set = pages
+    if page < half:
+        first_page_in_set = 1
+        last_page_in_set = min((pages_in_set, pages))
+    elif page > (pages-half):
+        first_page_in_set = max((pages-pages_in_set, 1))
+        last_page_in_set = pages
+    else:
+        first_page_in_set = max((page-half, 1))
+        last_page_in_set = page+half
 
     return render_template('search.html', q=q, page=page, results=results, \
             results_per_page=results_per_page, pages=pages, first_page_in_set=first_page_in_set, last_page_in_set=last_page_in_set, \
             quote=quote, comma=comma, int=int, facet_fields=facet_fields, lang_map=lang_map, facet_args=facet_args, \
-            get_movie_thumb=get_movie_thumb, year_gap=year_gap, find_item=find_item, enumerate=enumerate, len=len, pick_best=pick_best, url=url, facet_args_dict=facet_args_dict,\
+            get_movie_thumb=get_movie_thumb, year_gap=year_gap, find_item=find_item, enumerate=enumerate, len=len, pick_best=pick_best, url=url, \
+            facet_args_dict=facet_args_dict,\
             get_img_thumb = get_img_thumb, changequery=changequery, token_hl=token_hl)
     
 if __name__ == "__main__":
